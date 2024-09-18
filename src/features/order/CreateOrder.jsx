31319f3a89d7cb51fa21c903abbe837b298eec3a
abid982 <abid982@gmail.com>
2024-09-18 11:46:03 +0500

    implement react router to fetch and manage data & design pages with tailwind css

---
+++ src/features/order/CreateOrder.jsx
@@ -0,0 +1,144 @@
+// import { useState } from 'react';
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
+import { createOrder } from '../../services/apiRestaurant';
+import Button from '../../ui/Button';
+
+// https://uibakery.io/regex-library/phone-number
+const isValidPhone = str =>
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str,
+  );
+
+const fakeCart = [
+  {
+    pizzaId: 12,
+    name: 'Mediterranean',
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+  {
+    pizzaId: 6,
+    name: 'Vegetale',
+    quantity: 1,
+    unitPrice: 13,
+    totalPrice: 13,
+  },
+  {
+    pizzaId: 11,
+    name: 'Spinach and Mushroom',
+    quantity: 1,
+    unitPrice: 15,
+    totalPrice: 15,
+  },
+];
+
+function CreateOrder() {
+  const navigation = useNavigation();
+  console.log(navigation);
+
+  const isSubmitting = navigation.state === 'submitting';
+
+  const formErrors = useActionData();
+
+  console.log(formErrors);
+
+  // const [withPriority, setWithPriority] = useState(false);
+
+  // CART DATA WILL COME FROM REDUX LATER ON
+  const cart = fakeCart;
+
+  return (
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">
+        Ready to order? Let&apos;s go!
+      </h2>
+
+      {/* POST, PATCH, DELETE */}
+      {/* <Form method="POST" action='/order/new'> */}
+      <Form method="POST">
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input type="text" name="customer" className="input grow" required />
+        </div>
+
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input type="tel" name="phone" className="input w-full" required />
+            {formErrors?.phone && (
+              <p className="mt-2 rounded bg-red-100 p-2 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
+          </div>
+        </div>
+
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              type="text"
+              name="address"
+              className="input w-full"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="mb-12 flex items-center gap-5">
+          <input
+            type="checkbox"
+            name="priority"
+            id="priority"
+            className="foucus:ring foucs:ring-yellow-400 h-6 w-6 accent-yellow-400 focus:outline-none focus:ring-offset-2"
+            // value={withPriority}
+            // onChange={(e) => setWithPriority(e.target.checked)}
+          />
+          <label htmlFor="priority" className="font-medium">
+            Want to give your order priority?
+          </label>
+        </div>
+
+        <div>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <Button type="primary" disabled={isSubmitting}>
+            {isSubmitting ? 'Placing order...' : 'Order now'}
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  console.log(data);
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === 'on',
+  };
+
+  console.log(order);
+
+  const errors = {};
+
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      'Please give us your correct phone number. We might need it to contact you.';
+
+  if (Object.keys(errors).length > 0) return errors;
+
+  // If everything is okay, create new order and redirect
+  const newOrder = await createOrder(order);
+
+  console.log(newOrder);
+
+  // return null;
+  return redirect(`/order/${newOrder.id}`);
+}
+
+export default CreateOrder;
